4f950c3f379ef83c9d5e376f8f1423fd0bbb718b
Nazar <furnoxl@yandex.ru>
2023-06-24 23:12:32 +0700

    Change ip

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const isProduction = process.env.NODE_ENV === "production"
 const instance = axios.create({
-  baseURL: `http://${isProduction ? 'localhost' : 'localhost:8000'}/api`,
+  baseURL: `http://${isProduction ? '31.129.97.140' : 'localhost:8000'}/api`,
 });
 
 export default instance;
