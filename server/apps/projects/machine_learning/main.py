4874cb681b32c0733aa0b0c405e014d99edf5b3a
Nazar <furnoxl@yandex.ru>
2023-06-25 13:28:59 +0700

    commit

--- server/apps/projects/machine_learning/main.py
+++ server/apps/projects/machine_learning/main.py
@@ -1,6 +1,7 @@
 """Главный модуль подсистемы"""
 
 import os
+import json
 
 import pandas as pd
 
@@ -22,18 +23,24 @@ DATA_PATH = os.path.join(DIR_NAME, 'data_baseline', 'dataset_hackaton_ksg.csv')
 
 def main(data_path):
     data_with_predict = predict_result_regression(MODEL_PATH, data_path)
-    result = get_report_dataframe(
-        data_with_predict,
-        ['Кодзадачи', 'НазваниеЗадачи', 'predictions'],
-    )
-    save_dataframe_xlsx(data_with_predict)
-    save_dataframe_csv(data_with_predict)
-    save_predictions_to_txt(data_with_predict['predictions'])
     ended_json = dataframe_to_json(data_with_predict)
     ended_json["tasks"] = get_tasks_and_names_json(
         data_with_predict["Кодзадачи"],
         data_with_predict["НазваниеЗадачи"],
     )
+    data_with_predict['predictions'] = abs(
+        data_with_predict['target'] - data_with_predict['predictions']
+    ).astype('Int64')
+    save_dataframe_xlsx(
+        data_with_predict,
+        filename='test_predictions.xlsx',
+    )
+    save_dataframe_csv(
+        data_with_predict,
+        filename='test_predictions.csv',
+    )
+    save_predictions_to_txt(
+        data_with_predict['predictions'], filename='test_predictions.txt')
     return ended_json
 
 
